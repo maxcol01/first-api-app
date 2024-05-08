b672b44f407cbb593f79f60dc520ab089b4c5929
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-05-08 19:27:51 +0200

    modification of HTML (change writing in p), CSS (change some style) and JS (add the position after fetching)

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(".button").on("click", () => {
     // fetch the API
-    $("#result").html("This app shows the current position of the International Space Station")
+    //$("#result").html("This app shows the current position of the International Space Station")
+    fetch('http://api.open-notify.org/iss-now.json')
+    .then(resp => resp.json())
+    .then(rslt => {
+        console.log(rslt.iss_position)
+       $("#result").html(`The latitude is ${rslt.iss_position.latitude}° and the longitude is ${rslt.iss_position.longitude}°`)
+    })
 });
 
