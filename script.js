754fc1bde625425fcf4dfe7f46e0e8ce493c812c
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-05-08 20:03:13 +0200

    final modifications for this exercice

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ $(".button").on("click", () => {
     .then(rslt => {
         console.log(rslt.iss_position)
        $("#result").html(`The latitude is ${rslt.iss_position.latitude}° and the longitude is ${rslt.iss_position.longitude}°`)
+       $(".exp").html("A negative latitude stands for the southern hemisphere and a negative longitude stands for the western hemisphere")
+       $("img").attr("src","https://i0.wp.com/printworldmaps.com/wp-content/uploads/2022/09/World-Map-with-Latitude-and-Longitude-PDF.jpg?resize=432%2C247&ssl=1")
     })
 });
 
