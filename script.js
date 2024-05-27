cd68033d6a7c446756147fe32b05dab3d9e4905f
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-05-27 19:29:14 +0200

    fixing the api call to https$

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(".button").on("click", () => {
     // fetch the API
     //$("#result").html("This app shows the current position of the International Space Station")
-    fetch('http://api.open-notify.org/iss-now.json')
+    fetch('https://api.wheretheiss.at/v1/satellites/25544')
     .then(resp => resp.json())
     .then(rslt => {
-        console.log(rslt.iss_position)
-       $("#result").html(`The latitude is ${rslt.iss_position.latitude}° and the longitude is ${rslt.iss_position.longitude}°`)
+       $("#result").html(`The latitude is ${rslt.latitude}° and the longitude is ${rslt.longitude}°`)
        $(".exp").html("A negative latitude stands for the southern hemisphere and a negative longitude stands for the western hemisphere")
        $("img").attr("src","https://i0.wp.com/printworldmaps.com/wp-content/uploads/2022/09/World-Map-with-Latitude-and-Longitude-PDF.jpg?resize=432%2C247&ssl=1")
     })
